f51aa469f2ae6ca1fdcbc67e4523df5500e3421f
ThyagoLucas <=>
2022-02-15 01:37:13 -0400

    Esqueleto da pagina 2 disabled, 50% concluido, e botão com o log das mensagens dos quizzes chegando do servidor

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+
+
+function getQuizzes(){
+    const quizzesServer = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    quizzesServer.then(response);    
+    ;
+}
+function response (data){
+    console.log(data.data);
+}
