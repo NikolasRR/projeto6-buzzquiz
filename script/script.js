24577898e5702a859cad1ec55706a7109b319314
ThyagoLucas <=>
2022-02-16 21:38:26 -0400

    commit de atualizaçao de codigos

--- script/script.js
+++ script/script.js
@@ -1,11 +1,28 @@
 
 function getQuizzes(){
-    const quizzesServer = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-    quizzesServer.then(loadQuizzes);    
+    const getQuizServer = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    getQuizServer.then(loadQuizzes); 
+    
     
 }
 function loadQuizzes(data){
-    console.log(data.data);
+    const allquizes = document.querySelector(".allQuizzes");
+    
+    for(let i = 0; i<data.length; i++){
+        object = data[i];
+        allquizes.innerHTML = `
+        <article class="quizz">
+            <img src="${object.image}">
+            <span>${object.title}</span>
+        </article>
+        
+        `
+
+        
+    }
+
+
+
 }
 
 
