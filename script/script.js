006fb0a3870482dd4e6f530f51b4a8928f40f117
ThyagoLucas <=>
2022-02-17 10:09:43 -0400

    Atualizaçao tela 2 na casa dos 70%

--- script/script.js
+++ script/script.js
@@ -36,35 +36,37 @@ function quizz(number){
 
     let quizzesOptions = document.querySelector(".options");
     quizzesOptions.innerHTML = ``;
-
+    
     for(let i = 0; i<object.questions.length; i++){
         quizzesOptions.innerHTML += `
-        <article class="option-quizz-selected">
-             <h1>${object.questions[i].title}</h1>
-            `
+            <article class="option-quizz-selected opt${i}">`
+            let choices = document.querySelector('.option-quizz-selected.opt'+i);
+            choices.innerHTML += `<h1>${object.questions[i].title}</h1>`
 
-
+                    
         for(let j = 0; j< object.questions[i].answers.length;j++){
+
             let response = object.questions[i].answers[j];
-            quizzesOptions.innerHTML += `
+            choices.innerHTML += `
             <figure class="option"> 
                 <img src="${response.image}">
                 <figcaption>${response.text}</figcaption>
             </figure>
             `
-        }
-
-        quizzesOptions.innerHTML +='</article>';
 
 
+            
+            
+        } 
+
+            
+
+    }
+    
         let disabled = document.querySelector(".screen1");
         disabled.classList.add("disabled");
         let enabled = document.querySelector(".screen2");
         enabled.classList.remove("disabled");
-
-    }
-    
-        
     
 
 
