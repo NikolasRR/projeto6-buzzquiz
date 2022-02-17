7130f1b626fbcc1b9bd8bb46d9a36e7526b9ff9b
ThyagoLucas <=>
2022-02-16 20:23:32 -0400

    commit test conclusao tela 2

--- script/script.js
+++ script/script.js
@@ -1,9 +1,12 @@
 
 function getQuizzes(){
     const quizzesServer = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-    quizzesServer.then(response);    
+    quizzesServer.then(loadQuizzes);    
     ;
 }
-function response (data){
+function loadQuizzes(data){
     console.log(data.data);
 }
+
+
+
