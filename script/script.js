cd43f075b353c26c26dd400c017f9df11adab71d
NikolasRR <nkls99@gmail.com>
2022-02-19 14:53:55 -0300

    botao voltar ao home adicionado

--- script/script.js
+++ script/script.js
@@ -482,12 +482,7 @@ function verifyAndGoToQuizzFinished () {
 
         const createdQuizzPromisse =  axios.post("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes", createdQuizz);
         createdQuizzPromisse.then(
-            response => {
-                createdQuizz.id = (response.data.id); 
-                const stringOfCreatedQuizz = JSON.stringify(createdQuizz);
-                localStorage.setItem(createdQuizz.title, stringOfCreatedQuizz);
-            });
-        
+            response => localStorage.setItem(createdQuizz.title, response.data.id));
         return
     }
     createdQuizz.levels = [];
